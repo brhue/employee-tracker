f7ca716ec622ceadd72f06267c815832979e8506
Bradley Donahue <donahue.bradley@gmail.com>
2021-01-20 18:39:52 -0500

    Add creating department/role/employee

--- index.js
+++ index.js
@@ -12,5 +12,49 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) throw err;
   console.log('connected!');
-  connection.end();
+  // createDepartment();
+  // createRole();
+  createEmployee();
 });
+
+function createDepartment() {
+  connection.query('insert into department (name) values (?)', ['Engineering'], (err, res) => {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+}
+
+function createRole() {
+  connection.query('select * from department', (err, rows) => {
+    if (err) throw err;
+    if (rows.length === 0) {
+      console.log('You need to create some departments first!');
+      connection.end();
+    } else {
+      const values = ['Lead Engineer', 200000, 1];
+      connection.query('insert into role (title, salary, department_id) values (?, ?, ?)', values, (err, res) => {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+      });
+    }
+  });
+}
+
+function createEmployee() {
+  connection.query('select * from role', (err, rows) => {
+    if (err) throw err;
+    if (rows.length === 0) {
+      console.log('You need to create some roles first!');
+      connection.end();
+    } else {
+      const values = ['Bradley', 'Donahue', 1];
+      connection.query('insert into employee (first_name, last_name, role_id) values (?, ?, ?)', values, (err, res) => {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+      });
+    }
+  });
+}
